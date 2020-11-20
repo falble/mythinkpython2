52ee879a8642410fac55ebfb51b876ea90cbd22b
Francesco Albertini <47675693+falble@users.noreply.github.com>
2020-11-20 16:15:20 +0100

    Add files via upload

---
+++ CAP17-Classes and methods/thinkpythonCAP17.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 23:43:51 2019
+
+@author: Falble
+"""
+
+# Chapter 17 - Classes and methods
+# the programs from the last two chapter are not really object-oriented because they don't represent the relationships
+# between programmer-definied types and the functions that operate on them. the next step is to transform those 
+# funtions into methods that make the relationships explicit.
+
+# 17.1 Object-oriented features 
