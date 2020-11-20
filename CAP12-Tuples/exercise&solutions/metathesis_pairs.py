0e2fae5bae970adebbb74e1ff179570ed6f6aa7e
Francesco Albertini <47675693+falble@users.noreply.github.com>
2020-11-20 16:05:30 +0100

    Add files via upload

---
+++ CAP12-Tuples/exercise&solutions/metathesis_pairs.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 23:26:25 2019
+
+@author: Falble
+"""
+
+import anagram_sets
+
+
+def metathesis_pairs(d):
+    """Print all pairs of words that differ by swapping two letters.
+    d: map from word to list of anagrams
+    """
+    for anagrams in d.values():
+        for word1 in anagrams:
+            for word2 in anagrams:
+                if word1 < word2 and word_distance(word1, word2) == 2:
+                    print(word1, word2)
+
+
+def word_distance(word1, word2):
+    """Computes the number of differences between two words.
+    word1, word2: strings
+    Returns: integer
+    """
+    assert len(word1) == len(word2)
+
+    count = 0
+    for c1, c2 in zip(word1, word2):
+        if c1 != c2:
+            count += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    sets = anagram_sets.all_anagrams('words.txt')
+    metathesis_pairs(sets)
