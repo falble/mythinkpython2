68dba6428c4952834ca6ef80622d729f6599d62a
Francesco Albertini <47675693+falble@users.noreply.github.com>
2020-11-20 15:43:39 +0100

    Add files via upload

---
+++ CAP1-The way of the program/thinkpythonCAP1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  8 19:26:06 2019
+
+@author: Utente
+"""
+#Chapter 2
+#compute tge of the houer that has elapsed 
+message = 'And now for something completely different'
+minute = 60 #write the minutes
+percentage = (minute * 100) / 60
+print(message, percentage,'%')
+
+#exercises
+#2.1
+n = 42 
+print(n*2)
+x = y = 1
+print(2*x,3*y)
